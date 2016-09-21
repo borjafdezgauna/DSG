2854c0d6fce1c339aeefacda50bd5d0549aada5e
Adrián <avazquez049@ikasle.ehu.es>
2016-09-21 15:27:42 +0200

    Añadido proyecto personal

---
+++ labs/Adrian/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <math.h>
+
+//OPENGL includes
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+//FreeGLUT includes
+#include "../packages/nupengl.core.0.1.0.1/build/native/include/GL/glut.h"
+#include "../packages/nupengl.core.0.1.0.1/build/native/include/GL/freeglut.h"
