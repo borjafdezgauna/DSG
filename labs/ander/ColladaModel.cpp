c3c1327f5571c4d3613a4823275c436b85a6acb8
josea_000 <josea_000@VACAS>
2016-11-02 17:55:01 +0100

    draw no funciona

--- labs/ander/ColladaModel.cpp
+++ labs/ander/ColladaModel.cpp
@@ -31,7 +31,7 @@ ColladaModel::ColladaModel(char* fileName)
 	tinyxml2::XMLElement* pVenomUV = pSource->NextSibling()->NextSibling()->FirstChildElement("float_array");
 	parseXMLFloatArray(pVenomUV, m_texCoords);
 
-	tinyxml2::XMLElement* pTriangles = pRoot->FirstChildElement("triangles")->FirstChildElement("input")->FirstChildElement("p");
+	tinyxml2::XMLElement* pTriangles = pMesh->FirstChildElement("triangles")->FirstChildElement("p");
 	parseXMLIntArray(pTriangles,m_index);
 
 }
@@ -43,15 +43,15 @@ ColladaModel::~ColladaModel()
 
 void ColladaModel::draw()
 {
+
 	glBegin(GL_TRIANGLES);
-	for (int j = 0; j <= m_positions.size(); j = j + 3)
+	int index = 0;
+	for (int i = 0; i < m_index.size(); i++)
 	{
-		for (int i = 0; i <= m_index.size(); i++)
-		{
-		
-		
-			glVertex3f(m_positions[j], m_positions[j+1], m_positions[j+2]);
-		}
+		index= m_index[i];
+		glNormal3f(m_normals[index*3], m_normals[index*3 + 1], m_normals[index*3 + 2]);
+		glTexCoord3f(m_texCoords[index *2], m_texCoords[index *2 + 1], m_texCoords[index *2 + 2]);
+		glVertex3f(m_positions[index *3], m_positions[index *3 +1], m_positions[index *3 + 2]);
 
 	}
 	glEnd();
