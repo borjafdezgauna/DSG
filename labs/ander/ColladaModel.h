617602142a59c732a1bbf4f1fd8542b4f3e0fe3c
josea_000 <josea_000@VACAS>
2016-11-02 17:04:05 +0100

    fdsfds

--- labs/ander/ColladaModel.h
+++ labs/ander/ColladaModel.h
@@ -1,10 +1,22 @@
 #pragma once
+#include <vector>
+#pragma comment(lib, "../../Debug/tinyxml2.lib")
+#include "../../tinyxml2/tinyxml2.h"
 class ColladaModel
 {
 	char* modelo;
+	std::vector<double> m_positions;
+	std::vector<double> m_normals;
+	std::vector<double> m_texCoords;
+	std::vector<int> m_index;
+	int textureId;
+
 public:
 	ColladaModel(char* fileName);
 	~ColladaModel();
 	void draw();
+	
+	void parseXMLFloatArray(tinyxml2::XMLElement * pFloatArray, std::vector<double>& vector);
+	void parseXMLIntArray(tinyxml2::XMLElement * pFloatArray, std::vector<int>& vector);
 };
 
